ac900e0cb4ac652363d10c6f3a6683233694c692
Neel Joshi <pranav.barot@qdevtechnolab.com>
2026-05-27 18:42:20 +0530

    Updated Try-Catch code

--- src/Practicle18.java
+++ src/Practicle18.java
@@ -1,18 +1,46 @@
-public class Practicle18 {
-	final int num = 10;
-	public static void main(String[] args){
-		Practicle18 eh = new Practicle18();
-		eh.checkException();
+class ExceptionDemo {
+
+	// throws keyword
+	static void checkAge(int age) throws ArithmeticException {
+
+		// throw keyword
+		if (age < 18) {
+			throw new ArithmeticException("Not eligible for voting");
+		} else {
+			System.out.println("Eligible for voting");
+		}
 	}
+}
 
-	void checkException(){
-		try{
-			int result = num / 0;
+public class Practicle18 {
 
-		} catch (Exception e) {
-			System.out.println("Exception Throws : Divisible by Zero ");
-		}finally {
-			System.out.println("Finally Block Always Executes");
+	public static void main(String[] args) {
+
+		try {
+
+			// Exception generating code
+			int number = 10 / 0;
+
+			System.out.println(number);
+
+		} catch (ArithmeticException e) {
+
+			// catch block
+			System.out.println("Exception Caught: " + e);
+
+		} finally {
+
+			// finally block
+			System.out.println("Finally block always executes");
+		}
+
+		// Calling method
+		try {
+			ExceptionDemo.checkAge(15);
+
+		} catch (ArithmeticException e) {
+
+			System.out.println("Custom Exception: " + e.getMessage());
 		}
 	}
 
