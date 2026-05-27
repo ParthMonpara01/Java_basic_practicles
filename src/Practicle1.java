d9d453f1c00521bcd1757995f58926eaced5c5e4
Neel Joshi <pranav.barot@qdevtechnolab.com>
2026-05-27 16:20:52 +0530

    .

--- src/Practicle1.java
+++ src/Practicle1.java
@@ -1,5 +1,8 @@
 public class Practicle1 {
-	public static void main(String[] args){
-		System.out.println("Hello World");
+
+	public static void main(String[] args) {
+		//Writing a programm to print Hello World
+		System.out.println("First Java programm");
 	}
+
 }
