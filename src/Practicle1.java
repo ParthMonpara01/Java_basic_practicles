56b38224b9c23f9d857a45c03138d75fd4f30922
Parth Monpara <parth.monpara@qdevtechnolab.com>
2026-05-30 13:08:17 +0530

    .

--- src/Practicle1.java
+++ src/Practicle1.java
@@ -2,7 +2,7 @@ public class Practicle1 {
 
 	public static void main(String[] args) {
 		//Writing a programm to print Hello World
-		System.out.println("First Java programm");
+		System.out.println("First Java programm..");
 	}
 
 }
